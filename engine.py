50be6caa4ed634f3ffb54f77ae37a2576101e46e
Alok589 <alokchauhan589@gmail.com>
2020-12-22 12:15:08 +0000

    updated engine.py

--- engine.py
+++ engine.py
@@ -1,3 +1,4 @@
+ 
 # engine.py
 import torch
 import torch.nn as nn
@@ -5,12 +6,12 @@ from tqdm import tqdm
 # import model
 from torch._C import device
 from torch.optim import optimizer
-
+import numpy as np
 
 def train(data_loader, model, optimizer, device):
 
     model.train()
-
+    batch_MSEs = []
     for data in tqdm(data_loader):
         # remember, we have image and targets
         # in our dataset class
@@ -25,11 +26,17 @@ def train(data_loader, model, optimizer, device):
         outputs = model(inputs)
         # calculate loss
         loss = nn.MSELoss()(outputs, targets)
+        batch_MSEs.append(loss.item())
         # backward step the loss
         loss.backward()
         # step optimizer
         optimizer.step()
-
+        #print(loss.item())
+    batch_MSEs = np.array(batch_MSEs)        
+    epoch_loss = np.mean(batch_MSEs)
+    print(epoch_loss)
+    return epoch_loss    
+        
 
 def evaluate(data_loader, model, device):
 
@@ -39,26 +46,30 @@ def evaluate(data_loader, model, device):
     # :param model: pytorch model
     # :param device: cuda/cpu
     # """
-    # put model in evaluation mode
-    model.eval()
-    # init lists to store targets and outputs
-    final_targets = []
-    final_outputs = []
 
+    # put model in evaluation mode
+    print("_____________validation_____________")
+    model.eval()
+
+    # init lists to store targets and outputs
+    batch_MSEs = []
     # we use no_grad context
     with torch.no_grad():
-        for data in data_loader:
+        for idx, data in enumerate(data_loader, 1):
             inputs = data["image"]
             targets = data["targets"]
             inputs = inputs.to(device, dtype=torch.float)
             targets = targets.to(device, dtype=torch.float)
             # do the forward step to generate prediction
             output = model(inputs)
+
             # convert targets and outputs to lists
-            targets = targets.detach().cpu().numpy().tolist()
-            output = output.detach().cpu().numpy().tolist()
-            # extend the original list
-            final_targets.extend(targets)
-            final_outputs.extend(output)
+            batch_mse = ((output-targets)**2).mean().item()
+            #print("batch"+str(idx) + " loss:" ,batch_mse)
+
+            batch_MSEs.append(batch_mse)
             # return final output and final targets
-    return final_outputs, final_targets
+    batch_MSEs = np.array(batch_MSEs)        
+    epoch_loss = np.mean(batch_MSEs)
+    print(epoch_loss)
+    return epoch_loss
